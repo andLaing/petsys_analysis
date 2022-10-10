e6f225911875992d624b66690a2224f44f23318f
andLaing <a.b.r.laing@gmail.com>
2022-10-10 16:33:13 +0200

    Add floodmap and CTR to plots.py

--- pet_code/scripts/flood_maps.py
+++ pet_code/scripts/flood_maps.py
@@ -1,7 +1,14 @@
 import sys
 
-from pet_code.src.io   import read_petsys, read_ymlmapping
-from pet_code.src.util import filter_impact
+import matplotlib.pyplot as plt
+import numpy  as np
+import pandas as pd
+
+from pet_code.src.fits  import fit_gaussian
+from pet_code.src.io    import read_petsys, read_ymlmapping
+from pet_code.src.plots import mm_energy_spectra, group_times
+from pet_code.src.util  import filter_impact, get_supermodule_eng
+from pet_code.src.util  import centroid_calculation, filter_multihit
 
 
 def read_and_filter(eng_map, min_sm1, min_sm2):
@@ -13,11 +20,27 @@ def read_and_filter(eng_map, min_sm1, min_sm2):
     """
     m1_filter = filter_impact(min_sm1, eng_map)
     m2_filter = filter_impact(min_sm2, eng_map)
-    # def valid_event(evt_tuple):
-    #     return m1_filter(evt_tuple[0]) and m2_filter(evt_tuple[1])
     def valid_event(sm1, sm2):
-        return m1_filter(sm1) and m2_filter(sm2)
+        return filter_multihit(sm1) and filter_multihit(sm2) and m1_filter(sm1) and m2_filter(sm2)
+        # return m1_filter(sm1) and m2_filter(sm2)
     return valid_event
+
+
+def module_centroids(event, c_calc):
+    """
+    Get all relevant centroids for each super module in the event.
+    Muy cutre ahora.
+    """
+    sm1_dict = pd.DataFrame(event[0], columns=['id', 'mm', 'tstp', 'eng']).groupby('mm').apply(lambda x: c_calc(x.values)).to_dict()
+    sm2_dict = pd.DataFrame(event[1], columns=['id', 'mm', 'tstp', 'eng']).groupby('mm').apply(lambda x: c_calc(x.values)).to_dict()
+    return sm1_dict, sm2_dict
+
+
+# def range_filter(val, vmin, vmax):
+#     """
+#     Select
+#     """
+
 
 import time
 if __name__ == '__main__':
@@ -26,11 +49,45 @@ if __name__ == '__main__':
     map_file  = 'pet_code/test_data/SM_mapping.yaml' # shouldn't be hardwired
     file_list = sys.argv[1:]
 
-    time_ch, eng_ch, mm_map, centroid_map = read_ymlmapping(map_file)
+    time_ch, eng_ch, mm_map, centroid_map, _ = read_ymlmapping(map_file)
     evt_select = read_and_filter(eng_ch, 5, 4) # minima should not be hardwired
     pet_reader = read_petsys(mm_map, evt_select)
     # filtered_events = list(filter(evt_select, pet_reader(file_list)))
     filtered_events = [tpl for tpl in pet_reader(file_list)]
+    # filtered_events = pd.DataFrame((v for tpl in pet_reader(file_list) for v in np.vstack(tpl)),
+    #                                columns=["event", "sm", "ch_id", "mm", "tstp", "energy"])
     end_r = time.time()
     print("Time enlapsed reading: {} s".format(int(end_r - start)))
     print("length check: ", len(filtered_events))
+    ## Should we be filtering the events with multiple mini-modules in one sm?
+    c_calc = centroid_calculation(centroid_map)
+    # ## Must be a better way but...
+    mod_dicts = [{}, {}]
+    for evt in filtered_events:
+        for i, ((x, y, _), (_, eng)) in enumerate(zip(map(c_calc, evt), map(get_supermodule_eng, evt, [eng_ch] * 2))):
+            mm = evt[0][i][1]
+            try:
+                mod_dicts[i][mm]['x'].append(x)
+                mod_dicts[i][mm]['y'].append(y)
+                mod_dicts[i][mm]['energy'].append(eng)
+            except KeyError:
+                mod_dicts[i][mm] = {'x': [x], 'y': [y], 'energy': [eng]}
+
+    # ## No file separation in case of multiple files, needs to be fixed.
+    # print("Stats check: ", l1, ", ", l2)
+    photo_peak = list(map(mm_energy_spectra, mod_dicts, [1, 2], [file_list[0]] * 2))
+
+    ## CTR. Reference to a slab in sm2
+    reco_dt = group_times(filtered_events, photo_peak, eng_ch, time_ch, 1)
+    ref_count = 0
+    for ref_ch, tstps in reco_dt.items():
+        #print("length = ", len(tstps))
+        ref_count += 1
+        time_arr = np.array(tstps)
+        plt.hist(np.diff(time_arr[:, 1:], axis=1).flatten(), bins=151, range=[-5000, 5000], histtype='step', fill=False, label = f"Ref ch {ref_ch}")
+        plt.xlabel(f'tstp ch {ref_ch} - tstp coinc')
+        # plt.show()
+    print("Ref channels found: ", ref_count)
+            
+
+
--- pet_code/src/plots.py
+++ pet_code/src/plots.py
@@ -1,5 +1,9 @@
 import matplotlib.pyplot as plt
 
+from . fits import fit_gaussian
+from . util import get_supermodule_eng
+from . util import np
+
 def plot_settings():
     plt.rcParams[ 'lines.linewidth' ] =  2
     plt.rcParams[ 'font.size'       ] = 11
@@ -15,3 +19,115 @@ def hist1d(axis, data, bins=200, range=(0, 300), histtype='step', label='histo')
     """
     weights, pbins, _ = axis.hist(data, bins=bins, range=range, histtype=histtype, label=label)
     return pbins, weights
+
+
+def mm_energy_spectra(module_xye, sm_label, plot_output=None):
+    """
+    Generate the energy spectra and select the photopeak
+    for each module. Optionally plot and save spectra
+    and floodmaps.
+
+    module_xye : Dict
+                 Supermodule xyz lists for each mm
+                 key is mm number (1--),
+    return
+                 List of energy selection filters.
+    """
+    photo_peak = []
+    if plot_output:
+        plot_settings()
+        fig, axes = plt.subplots(nrows=4, ncols=4, figsize=(15, 15))
+        xfilt = None
+        yfilt = None
+        for j, ax in enumerate(axes.flatten()):
+            ## mmini-module numbers start at 1
+            bin_edges, bin_vals = hist1d(ax, module_xye[j+1]['energy'], label=f'Det: {sm_label}\n mM: {j+1}')
+            try:
+                bcent, gvals, pars, cov = fit_gaussian(bin_vals, bin_edges, cb=6)
+                minE, maxE = pars[1] - 2 * pars[2], pars[1] + 2 * pars[2]
+            except RuntimeError:
+                minE, maxE = 0, 300
+            eng_arr = np.array(module_xye[j+1]['energy'])
+            photo_peak.append(lambda eng_val: (eng_val > minE) & (eng_val < maxE))
+            ax.plot(bcent, gvals, label=f'fit $\mu$ = {round(pars[1], 3)},  $\sigma$ = {round(pars[2], 3)}')
+            ax.set_xlabel('Energy (au)')
+            ## Filters for floodmaps
+            ax.axvspan(minE, maxE, facecolor='#00FF00' , alpha = 0.3, label='Selected range')
+            ax.legend()
+            ## Filter the positions
+            if xfilt is not None:
+                xfilt = np.hstack((xfilt, np.array(module_xye[j+1]['x'])[photo_peak[-1](eng_arr)]))
+                yfilt = np.hstack((yfilt, np.array(module_xye[j+1]['y'])[photo_peak[-1](eng_arr)]))
+            else:
+                xfilt = np.array(module_xye[j+1]['x'])[photo_peak[-1](eng_arr)]
+                yfilt = np.array(module_xye[j+1]['y'])[photo_peak[-1](eng_arr)]
+        fig.savefig(plot_output.replace(".ldat","_EnergyModuleSMod" + str(sm_label) + ".png"))
+        plt.clf()
+        plt.hist2d(xfilt, yfilt, bins = 500, range=[[0, 104], [0, 104]], cmap="Reds", cmax=250)
+        plt.xlabel('X position (pixelated) [mm]')
+        plt.ylabel('Y position (monolithic) [mm]')
+        plt.colorbar()
+        plt.tight_layout()
+        plt.savefig(plot_output.replace(".ldat","_FloodModule" + str(sm_label) + ".png"))
+        plt.clf()
+    else:
+        for j in range(1, 17):
+            bin_vals, bin_edges = np.histogram(module_xye[j]['energy'], bins=200, range=(0, 300))
+            try:
+                *_, pars, _ = fit_gaussian(bin_vals, bin_edges, cb=6)
+                minE, maxE = pars[1] - 2 * pars[2], pars[1] + 2 * pars[2]
+            except RuntimeError:
+                minE, maxE = 0, 300
+            photo_peak.append(lambda eng_val: (eng_val > minE) & (eng_val < maxE))
+    return photo_peak
+
+
+def group_times(filtered_events, peak_select, eng_ch, time_ch, ref_supermod):
+    """
+    Group the first time signals for each slab
+    in a reference super module with all slabs
+    in other supermodules.
+    filered_events: List of tuple of coincidence lists
+                    The information for each event in structure
+                    ([[id, mm, tstp, eng], ...], [...])
+    peak_select   : List of Lists
+                    Energy filter functions for each minimodule
+    eng_ch        : Set
+                    Set of all channels for energy measurement.
+    time_ch       : Set
+                    Set of all channels for time measurement.
+    ref_supermod  : int
+                    Index (0 or 1) of the reference SM.
+    returns
+    A dictionary of tuples (coinc id, coinc tstp, ref tstp)
+    with key ref id.
+    """
+    reco_dt  = {}
+    coinc_sm = 0 if ref_supermod == 1 else 1
+    min_ch   = [0, 0]
+    for sm1, sm2 in filtered_events:
+        mm1   = sm1[0][1]
+        _, e1 = get_supermodule_eng(sm1, eng_ch)
+        mm2   = sm2[0][1]
+        _, e2 = get_supermodule_eng(sm2, eng_ch)
+        if peak_select[0][mm1-1](e1) and peak_select[1][mm2-1](e2):
+            try:
+                min_ch[0] = next(filter(lambda x: x[0] in time_ch, sm1))
+            except StopIteration:
+                continue
+            try:
+                min_ch[1] = next(filter(lambda x: x[0] in time_ch, sm2))
+            except StopIteration:
+                continue
+            ## Want to know the two channel numbers and timestamps.
+            try:
+                # Key is the reference channel/Slab, each has a
+                # list of lists with [ch_other, t_other, t_ref]
+                reco_dt[min_ch[ref_supermod][0]].append([min_ch[coinc_sm    ][0],
+                                                         min_ch[coinc_sm    ][2],
+                                                         min_ch[ref_supermod][2]])
+            except KeyError:
+                reco_dt[min_ch[ref_supermod][0]] = [[min_ch[coinc_sm    ][0],
+                                                     min_ch[coinc_sm    ][2],
+                                                     min_ch[ref_supermod][2]]]
+    return reco_dt
