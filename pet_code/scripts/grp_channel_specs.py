d68ea8c7618888b029a9a7c755940216d1c0aafe
andLaing <a.b.r.laing@gmail.com>
2022-11-15 15:09:05 +0100

    Tidy grp_channel_specs

--- pet_code/scripts/grp_channel_specs.py
+++ pet_code/scripts/grp_channel_specs.py
@@ -30,18 +30,16 @@ from pet_code.src.util import shift_to_centres
 
 
 class ChannelCal:
-    def __init__(self, time_ch, eng_ch, mm_indx) -> None:
+    def __init__(self, time_ch, eng_ch) -> None:
         self.slab_energies = {}
         self.eng_sum       = {}# Mod eng channel sum with max slab key
         self.eng_max       = {}# Eng channel spectra for max eng channel.
         self.time_id       = time_ch
         self.eng_id        = eng_ch
-        self.mm_indx       = mm_indx
 
     def add_evt(self, supermods):
         mms     = {ch[1] for ch in supermods[0]}
         sel_mod = select_module(supermods[0], self.eng_id)
-        mm      = sel_mod[0][1]
         try:
             t_ch = next(filter(lambda ch: ch[0] in self.time_id, sel_mod))
         except StopIteration:
@@ -61,80 +59,6 @@ class ChannelCal:
         except KeyError:
             self.eng_max[max_eng[0]] = [max_eng[3]]
         return len(mms)
-
-
-def channel_energies(eng_ch, time_ch):
-    specs = {}
-    def add_value(channels):
-        for impact in channels[0]:
-            try:
-                specs[impact[1]][impact[0]].append(impact[3])
-            except KeyError:
-                try:
-                    specs[impact[1]][impact[0]] = [impact[3]]
-                except KeyError:
-                    specs[impact[1]] = {impact[0]: [impact[3]]}
-    def add_mm_max(channels):
-        arr_imps = np.asarray(channels[0])
-        # Shite code, improve
-        for mm in np.unique(arr_imps[:, 1]):
-            mm_info  = arr_imps[arr_imps[:, 1] == mm]
-            max_vals = mm_info[np.argmax(mm_info[:, 3])]
-            try:
-                specs[mm][max_vals[0]].append(max_vals[3])
-            except KeyError:
-                try:
-                    specs[mm][max_vals[0]] = [max_vals[3]]
-                except KeyError:
-                    specs[mm] = {max_vals[0]: [max_vals[3]]}
-    def add_max_mm(channels):
-        max_mm = select_module(channels[0])
-        mm_id  = max_mm[0][1]
-        for impact in max_mm:
-            try:
-                specs[mm_id][impact[0]].append(impact[3])
-            except KeyError:
-                try:
-                    specs[mm_id][impact[0]] = [impact[3]]
-                except KeyError:
-                    specs[mm_id] = {impact[0]: [impact[3]]}
-    def add_engtime_max(channels):
-        """
-        Only use the max of the energy channels and that of time ch
-        separately.
-        """
-        e_chans   = list(map(lambda x: x[3], filter(lambda x: x[0] in  eng_ch, channels[0])))
-        t_chans   = list(map(lambda x: x[3], filter(lambda x: x[0] in time_ch, channels[0])))
-        
-        if e_chans:
-            eng_max  = channels[0][np.argmax(e_chans)]
-            try:
-                specs[eng_max[1]][eng_max[0]].append(eng_max[3])
-            except KeyError:
-                try:
-                    specs[eng_max[1]][eng_max[0]] = [eng_max[3]]
-                except KeyError:
-                    specs[eng_max[1]] = {eng_max[0]: [eng_max[3]]}
-        if t_chans:
-            time_max = channels[0][np.argmax(t_chans)]
-            try:
-                specs[time_max[1]][time_max[0]].append(time_max[3])
-            except KeyError:
-                try:
-                    specs[time_max[1]][time_max[0]] = [time_max[3]]
-                except KeyError:
-                    specs[time_max[1]] = {time_max[0]: [time_max[3]]}
-
-    def loop_channels():
-        for ids in specs.values():
-            for id, engs in ids.items():
-                yield id, engs
-    def loop_mms():
-        for mm, vals in specs.items():
-            yield mm, vals
-    # return add_value, loop_channels, loop_mms#get_spec
-    # return add_mm_max, loop_channels, loop_mms
-    return add_engtime_max, loop_channels, loop_mms
 
 
 def filter_minch(min_ch, eng_ch):
@@ -165,13 +89,9 @@ if __name__ == '__main__':
     infiles  = args['INPUT']
 
     time_ch, eng_ch, mm_map, _, _ = read_ymlmapping(map_file)
-    # add_val, spec_loop, mm_loop   = channel_energies()
-    # add_val, spec_loop, mm_loop   = channel_energies(eng_ch, time_ch)
-    filt = filter_minch(4, eng_ch)
-    # filt = filter_oneMM()
-    # plotter = ChannelCal(time_ch, mm_index)
-    plotS  = ChannelCal(time_ch, eng_ch, mm_index)
-    plotNS = ChannelCal(time_ch, eng_ch, mm_index)
+    filt   = filter_minch(4, eng_ch)
+    plotS  = ChannelCal(time_ch, eng_ch)
+    plotNS = ChannelCal(time_ch, eng_ch)
     # Maybe a bit dangerous memory wise, review
     for fn in infiles:
         reader = read_petsys_filebyfile(fn, mm_map, sm_filter=filt, singles=True)
@@ -180,9 +100,6 @@ if __name__ == '__main__':
             num_mmsN = tuple(map(plotNS.add_evt, reader()))
         else:
             num_mmsS = tuple(map(plotS .add_evt, reader()))
-        # reader  = read_petsys_filebyfile(fn, mm_map, singles=True)
-        # _      = tuple(map(add_val, reader()))
-        # num_mms = tuple(map(plotter.add_evt, reader()))
     print("First pass complete, mm multiplicities with    Source: ", Counter(num_mmsS))
     print("First pass complete, mm multiplicities without Source: ", Counter(num_mmsN))
     spec_bins = np.arange(5, 30, 0.2)
@@ -191,7 +108,7 @@ if __name__ == '__main__':
         for id, engs in plotS.slab_energies.items():
             s_vals , bin_e = np.histogram(engs, bins=spec_bins)
             try:
-                ns_vals, _     = np.histogram(plotNS.slab_energies[id], bins=spec_bins)
+                ns_vals, _ = np.histogram(plotNS.slab_energies[id], bins=spec_bins)
             except KeyError:
                 plt.plot(bin_e[:-1], s_vals , label='Source')
                 plt.legend()
@@ -221,9 +138,6 @@ if __name__ == '__main__':
             plt.xlabel(f'Energy time channel {id}')
             plt.ylabel('source spec - no source spec (au)')
             plt.savefig(out_file + f'BackRest_ch{id}.png')
-        # plt.hist(engs, bins=np.arange(0, 30, 0.2))
-        # plt.xlabel(f'Energy time channel {id}')
-        # plt.savefig(out_file  + f'{id}.png')
             plt.clf()
     sm_add = (0, 512)
     esum_bins = np.arange(0, 300, 1.5)
@@ -260,20 +174,3 @@ if __name__ == '__main__':
             plt.legend()
             plt.savefig(out_file + f'Emax_ch{id}.png')
             plt.clf()
-
-
-    # for id, engs in spec_loop():
-    #     plt.hist(engs, bins=np.arange(0, 30, 0.2))
-    #     ch_type = 'time' if id in time_ch else 'energy'
-    #     plt.xlabel(f'Energy {ch_type} channel {id}')
-    #     plt.savefig(out_file  + f'{id}.png')
-    #     plt.clf()
-    # # The following isn't really what we're after.
-    # if mm_spec:
-    #     for mm, engs in mm_loop():
-    #         all_engs = np.concatenate([e for _, e in filter(lambda k: k[0] in eng_ch, engs.items())])
-    #         plt.hist(all_engs, np.arange(0, 30, 0.2))
-    #         plt.xlabel(f'Energy spec all channels mm{mm}')
-    #         plt.savefig(out_file + f'mm{mm}.png')
-    #         plt.clf()
-
