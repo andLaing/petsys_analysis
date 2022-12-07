074f6f988e79945f847a6fd5373e06d2fda5078b
andLaing <a.b.r.laing@gmail.com>
2022-12-07 12:01:56 +0100

    Add newline at end of setup file

--- setup.py
+++ setup.py
@@ -1,4 +1,7 @@
 from setuptools   import setup, find_packages
 from Cython.Build import cythonize
 
-setup(name = 'pet_code', packages = find_packages(), ext_modules = cythonize('pet_code/src/io_util.pyx'))
+setup(name        = 'pet_code'     ,
+      packages    = find_packages(),
+      ext_modules = cythonize('pet_code/src/io_util.pyx', compiler_directives={'language_level' : "3"})
+     )
