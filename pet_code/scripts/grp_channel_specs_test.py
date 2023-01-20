3e7980c6c1880a7b72e7cafd9d04d50e43cfafe4
andLaing <a.b.r.laing@gmail.com>
2023-01-20 10:54:56 +0100

    Tidy grp_channel_specs_test

--- pet_code/scripts/grp_channel_specs_test.py
+++ pet_code/scripts/grp_channel_specs_test.py
@@ -30,17 +30,11 @@ def test_channel_plots(TEST_DATA_DIR, TMP_OUT):
     assert len(plotNS.tdist) == 246
     assert len(plotS .edist) == 252
     assert len(plotNS.edist) == 207
-    # assert len(plotS .sum_dist) == 255
-    # assert len(plotNS.sum_dist) == 246
     nval_Sslab   = sum(plotS .tdist.values()).sum()
     nval_woSslab = sum(plotNS.tdist.values()).sum()
     nval_Semax   = sum(plotS .edist.values()).sum()
     nval_woSemax = sum(plotNS.edist.values()).sum()
-    # nval_Sesum   = sum(plotS .sum_dist.values()).sum()
-    # nval_woSesum = sum(plotNS.sum_dist.values()).sum()
     assert nval_Sslab   == 8792
-    # assert nval_Sesum   == 8820
-    assert nval_Semax   == 8742
     assert nval_woSslab ==  829
-    # assert nval_woSesum ==  831
+    assert nval_Semax   == 8742
     assert nval_woSemax ==  824
