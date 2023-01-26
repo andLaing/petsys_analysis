f13bd02ba3a180eb12c38bbddb171eafb9917187
andLaing <a.b.r.laing@gmail.com>
2023-01-26 15:25:45 +0100

    Add single ring map generator to make_map

--- pet_code/scripts/make_map.py
+++ pet_code/scripts/make_map.py
@@ -1,23 +1,28 @@
 #!/usr/bin/env python3
 
-"""Make a dataframe with mapping info (for 2 SM IMAS!) and save
+"""Make a dataframe with mapping info for TBPET type SM and save
 
-Usage: make_map.py [-f NFEM] MAPYAML
+Usage: make_map.py [-f NFEM] [-g GEOM] [-o OUT] MAPYAML
 
 Arguments:
     MAPYAML  File name with time and energy channel info.
 
 Options:
-    -s=NSM     Number of Super modules [default: 4]
-    -f=NFEM    Number of channels per Supermodule [default: 256]
+    -f=NFEM  Number of channels per Supermodule [default: 256]
+    -g=GEOM  Geometry: 2SM, 1ring [default: 1ring]
+    -o=OUT   Path for output file [default: 1ring_map]
 """
 
 import yaml
 
 from docopt import docopt
 
+import numpy  as np
 import pandas as pd
 
+from scipy.spatial.transform import Rotation as R
+
+from pet_code.src.util import ChannelType
 from pet_code.src.util import slab_x, slab_y, slab_z
 
 
@@ -55,14 +60,108 @@ def row_gen(nFEM, chan_per_mm, tchans, echans):
             yield id, 'ENERGY', i, mm, x, y, z, pp, id
 
 
+def channel_sm_coordinate(mm_rowcol, ch_indx, type):
+    """
+    mm_rowcol : Either row or col depending on type: TIME row, ENERGY col.
+    ch_indx   : index along row/column
+    """
+    mm_spacing   = 0.3 * ((31 - ch_indx) // 8)
+    local_fine   = round(1.75 + mm_spacing + 3.2 * (31 - ch_indx), 2)
+    local_coarse = round(25.9 * (3.5 - mm_rowcol), 2)
+    if type is ChannelType.TIME:
+        # local y course, local_x fine
+        return local_fine, local_coarse
+    return local_coarse, local_fine
+
+
+def sm_centre_pos():
+    """
+    Gives polar angle position (and r; fixed)
+    for the centres of the SMs.
+    Currently set for ring positions of 
+    center inner face of TBPET supermodules.
+    """
+    SM_r  = 410.0
+    # This shouldn't be hardwired!!
+    # Note order Y, X for easy use with arctan2
+    SM_yx = { 0: ( -53.5157,  406.4924),  1: (  53.5157,  406.4924),
+              2: ( 156.9002,  378.7906),  3: ( 249.5922,  325.2749),
+              4: ( 325.2749,  249.5922),  5: ( 378.7906,  156.9002),
+              6: ( 406.4924,   53.5157),  7: ( 406.4924,  -53.5157),
+              8: ( 378.7906, -156.9002),  9: ( 325.2749, -249.5922),
+             10: ( 249.5922, -325.2749), 11: ( 156.9002, -378.7906),
+             12: (  53.5157, -406.4924), 13: ( -53.5157, -406.4924),
+             14: (-156.9002, -378.7906), 15: (-249.5922, -325.2749),
+             16: (-325.2749, -249.5922), 17: (-378.7906, -156.9002),
+             18: (-406.4924,  -53.5157), 19: (-406.4924,   53.5157),
+             20: (-378.7906,  156.9002), 21: (-325.2749,  249.5922),
+             22: (-249.5922,  325.2749), 23: (-156.9002,  378.7906)}
+    def get_rtheta(SM):
+        return SM_r, np.arctan2(*SM_yx[SM])
+    return get_rtheta
+
+
+def sm_gen(nFEM, chan_per_mm, tchans, echans, mm_emap):
+    def _sm_gen(sm_no):
+        for i, (tch, ech) in enumerate(zip(tchans, echans)):
+            id = tch + sm_no * nFEM
+            mm = i // chan_per_mm + 1
+            loc_x, loc_y = channel_sm_coordinate(i // 32, i % 32, ChannelType.TIME)
+            yield id, 'TIME', mm, loc_x, loc_y
+            id = ech + sm_no * nFEM
+            mm = mm_emap[mm]
+            loc_x, loc_y = channel_sm_coordinate(i // 32, i % 32, ChannelType.ENERGY)
+            yield id, 'ENERGY', mm, loc_x, loc_y
+    return _sm_gen
+
+
+def single_ring(nFEM, chan_per_mm, tchans, echans):
+    mM_energyMapping = {1:1,  2:5,  3:9 ,  4:13,  5:2,  6:6,  7:10,  8:14,
+                        9:3, 10:7, 11:11, 12:15, 13:4, 14:8, 15:12, 16:16}
+    sm_angle         = sm_centre_pos()
+    superm_gen       = sm_gen(nFEM, chan_per_mm, tchans, echans, mM_energyMapping)
+    # Hardwired, fix.
+    SM_half_len      = 103.6 / 2
+    def ring_gen():
+        local_cols = ['id', 'type', 'minimodule', 'local_x', 'local_y']
+        for sm in range(24):
+            sm_local                = pd.DataFrame((ch for ch in superm_gen(sm)),
+                                                    columns = local_cols        )
+            sm_local['supermodule'] = sm
+
+            sm_r, sm_ang     = sm_angle(sm)
+            ## Translate to XYZ relative to SM centre at X = R, Y = Z = 0.
+            sm_local['X']    = sm_r
+            sm_local['Y']    = sm_local.local_x              - SM_half_len
+            sm_local['Z']    = sm_local.local_y.values[::-1] - SM_half_len
+            ## Rotate to supermodule angular position.
+            coords           = ['X', 'Y', 'Z']
+            sm_rot           = R.from_euler('z', sm_ang)
+            sm_local[coords] = sm_local[coords].apply(sm_rot.apply, axis=1,
+                                                      result_type='broadcast')
+            yield sm_local
+    return pd.concat((sm for sm in ring_gen()), ignore_index=True)
+
+
 if __name__ == '__main__':
-    args = docopt(__doc__)
-    # nsuper = args['-s']
-    nFEM = int(args['-f'])
+    args    = docopt(__doc__)
+    nFEM    = int(args['-f'])
+    geom    =     args['-g']
+    outname =     args['-o']
+
+    if '.feather' not in outname:
+        outname += '.feather'
 
     with open(args['MAPYAML']) as map_buffer:
         channel_map = yaml.safe_load(map_buffer)
 
-    df = pd.DataFrame((row for row in row_gen(nFEM, 8, channel_map['time_channels'], channel_map['energy_channels'])),
-                      columns=['id', 'type', 'supermodule', 'minimodule', 'X', 'Y', 'Z', 'PLOTP', 'recID'])
-    df.to_feather('pet_code/test_data/twoSM_IMAS_map.feather')
+    if geom == '2SM':
+        df = pd.DataFrame((row for row in row_gen(nFEM, 8, channel_map['time_channels'], channel_map['energy_channels'])),
+                        columns=['id', 'type', 'supermodule', 'minimodule', 'X', 'Y', 'Z', 'PLOTP', 'recID'])
+    elif geom == '1ring':
+        df = single_ring(nFEM, 8, channel_map['time_channels'], channel_map['energy_channels'])
+    else:
+        print('Geometry not recognised')
+        exit()
+
+    df.to_feather(outname)
