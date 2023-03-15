f47f2e85a1d01a1bd7807ccb780925b3342aebca
andLaing <a.b.r.laing@gmail.com>
2023-03-15 11:06:34 +0100

    Add more module info to negatives.py

--- pet_code/scripts/negatives.py
+++ pet_code/scripts/negatives.py
@@ -7,9 +7,9 @@ import numpy as np
 from pet_code.src.filters import filter_event_by_impacts
 from pet_code.src.io      import ChannelMap
 from pet_code.src.io      import read_petsys_filebyfile
-from pet_code.src.io      import read_ymlmapping
 from pet_code.src.util    import ChannelType
 from pet_code.src.util    import select_max_energy
+from pet_code.src.util    import select_module
 
 
 def print_info(all_channels, neg_channels, ch_map):
@@ -37,11 +37,11 @@ if __name__ == '__main__':
 
     singles  = 'coinc' not in infile
     chan_map = ChannelMap(map_file)
-    # time_ch, eng_ch, mm_map, *_ = read_ymlmapping('pet_code/test_data/SM_mapping_corrected.yaml')
     evt_filter = filter_event_by_impacts(4, singles=singles)
     reader     = read_petsys_filebyfile(chan_map.ch_type, evt_filter, singles=singles)
 
     print(f'Checking for negative channels in {infile}')
+    sel_mod     = select_module(chan_map.get_minimodule)
     total_evt   = 0
     neg_in_sm1  = 0
     neg_in_sm2  = 0
@@ -50,10 +50,20 @@ if __name__ == '__main__':
     neg_ch2     = []
     neg_MMs1    = []
     neg_MMs2    = []
+    nNeg_sm1    = []
+    nNeg_sm2    = []
+    maxMM_sm1   = []
+    maxMM_sm2   = []
     for sm1, sm2 in reader(infile):
         total_evt += 1
         sm1_neg = list(filter(lambda x: x[3] < 0, sm1))
+        nNeg_sm1.append(len(sm1_neg))
         sm2_neg = list(filter(lambda x: x[3] < 0, sm2))
+        nNeg_sm2.append(len(sm2_neg))
+        if sm1_neg or sm2_neg:
+            mm_sel = tuple(map(sel_mod, (sm1, sm2)))
+            maxMM_sm1.append(chan_map.get_minimodule(mm_sel[0][0][0]))
+            maxMM_sm2.append(chan_map.get_minimodule(mm_sel[1][0][0]))
         if sm1_neg:
             neg_in_sm1 += 1
             neg_MMs1.extend(print_info(sm1, sm1_neg, chan_map))
@@ -68,5 +78,9 @@ if __name__ == '__main__':
     print(f'{neg_in_both} events have negatives in both SM')
     print(f'{neg_in_sm1} events with at least one negative first list(indx 0)')
     print(f'{neg_in_sm2} events with at least one negative second list(indx 1)')
+    print(f'First SM, Max MM when negative found: {Counter(maxMM_sm1)}')
+    print(f'Second SM, Max MM when negative found: {Counter(maxMM_sm2)}')
+    print(f'First SM nNeg count: {Counter(nNeg_sm1)}')
+    print(f'Second SM nNeg count: {Counter(nNeg_sm2)}')
     print(f'{np.unique(neg_ch1).shape[0]} of the channels in first SM have negatives, {Counter(neg_MMs1)} minimodules count.')
     print(f'{np.unique(neg_ch2).shape[0]} of the channels in second SM have negatives, {Counter(neg_MMs2)} minimodule count.')
