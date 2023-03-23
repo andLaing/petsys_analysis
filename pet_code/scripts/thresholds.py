d90baa42f47ea3de8092863445a24d5a18c30817
andLaing <a.b.r.laing@gmail.com>
2023-03-23 17:53:50 +0100

    Update thresholds.py for new formats

--- pet_code/scripts/thresholds.py
+++ pet_code/scripts/thresholds.py
@@ -8,7 +8,9 @@ import numpy  as np
 
 import matplotlib.pyplot as plt
 
-from pet_code.src.io   import read_ymlmapping
+# from pet_code.src.io   import read_ymlmapping
+from pet_code.src.io   import ChannelMap
+from pet_code.src.util import ChannelType
 from pet_code.src.util import get_electronics_nums
 
 
@@ -33,10 +35,11 @@ if __name__ == '__main__':
     eng_TOffset    = config.getint('offset', 'energyT2', fallback=0)
     ##
 
-    map_file     = 'pet_code/test_data/SM_mapping_corrected.yaml'
-    time_ch, eng_ch, *_ = read_ymlmapping(map_file)
+    map_file = config.get('mapping', 'map_file')
+    ch_types = ChannelMap(map_file).ch_type
+    # time_ch, eng_ch, *_ = read_ymlmapping(map_file)
 
-    timeCh = pd.read_csv(time_name, sep='\t ').set_index('ID')['MU']
+    timeCh = pd.read_csv(time_name, sep='\t').set_index('ID')['MU']
 
     time_mu = np.mean(timeCh)
     print(f'Time mu average value = {time_mu}')
@@ -60,7 +63,7 @@ if __name__ == '__main__':
     plt.show()
 
     ## Energy channels
-    engCh  = pd.read_csv(eng_name, sep='\t ').set_index('ID')['MU']
+    engCh  = pd.read_csv(eng_name, sep='\t').set_index('ID')['MU']
     eng_mu = np.mean(engCh)
     print(f'Energy mu average value = {eng_mu}')
     EEbin_wid   = round(eng_mu / (eng_Edefault + 0.5), 2)
@@ -81,20 +84,20 @@ if __name__ == '__main__':
     print('Thresh check: ', time_mu, ", EThresh = ", np.searchsorted(time_EThresh, time_mu), ", T1 = ", np.searchsorted(time_TThresh, time_mu))
 
     output = config.get('output', 'out_path')
-    # id_limit = max(max(time_ch), max(eng_ch))
-    id_limit = 767
     with open(output, 'w') as thresh_out:
         thresh_out.write('#portID\tslaveID\tchipID\tchannelID\tvth_t1\tvth_t2\tvth_e\n')
-        for id in range(0, id_limit + 1):
+        for id, typ in ch_types.items():# range(0, id_limit + 1):
             portID, slaveID, chipID, channelID = get_electronics_nums(id)
-            if   id in time_ch:
+            if   typ is ChannelType.TIME  :
+            # if   id in time_ch:
                 vth_t1, vth_t2, vth_e = time_T1default, time_T2default, time_Edefault
                 peak_pos = timeCh.get(id)
                 if peak_pos:
                     vth_t1 = max(0, np.searchsorted(time_TThresh, peak_pos) - 1 + time_TOffset)
                     vth_t2 = max(0, np.searchsorted(time_T2Thresh, peak_pos) - 1 + time_T2Offset)
                     vth_e  = max(0, np.searchsorted(time_EThresh, peak_pos) - 1 + time_EOffset)
-            elif id in  eng_ch:
+            elif typ is ChannelType.ENERGY:
+            # elif id in  eng_ch:
                 vth_t1, vth_t2, vth_e = eng_T1default, eng_T2default, eng_Edefault
                 peak_pos = engCh.get(id)
                 if peak_pos:
