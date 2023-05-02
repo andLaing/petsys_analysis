3effd1d4ce4414cf377054569a611067527a0df6
andLaing <a.b.r.laing@gmail.com>
2023-05-02 18:21:02 +0200

    Remove whitespace in filters.py

--- pet_code/src/filters.py
+++ pet_code/src/filters.py
@@ -198,18 +198,17 @@ def filter_max_coin_event(sm_map : Callable    ,
                           singles: bool = False
                           ) -> Callable:
     """
-    Filter event to select two SM 
+    Filter event to select two SM
     and no negative event.
     """
-    
+
     ch1_filter = filter_impact(min_ch)
     ch2_filter = ch1_filter
     if singles:
         ch2_filter = lambda x: True
     max_sm_filter = filter_max_sm(max_sm, sm_map)
-    
+
     def valid_event(sm1: list[list], sm2: list[list]) -> bool:
         return filter_negatives(sm1) and filter_negatives(sm2) and\
             max_sm_filter(sm1, sm2) and ch1_filter(sm1) and ch2_filter(sm2)
-    return valid_event 
-
+    return valid_event
